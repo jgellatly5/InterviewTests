0b794647af1fb227914179c41da1215ce5cccfb7
jgellatly5 <jgellatly5@gmail.com>
2020-02-26 20:15:44 -0800

    revising binary search

--- app/src/main/java/com/gallopdevs/interviewtests/algorithms/BinarySearch.java
+++ app/src/main/java/com/gallopdevs/interviewtests/algorithms/BinarySearch.java
@@ -2,31 +2,31 @@ package com.gallopdevs.interviewtests.algorithms;
 
 public class BinarySearch {
     public static int binarySearchIterative(int[] numbers, int target) {
-        int low = 0;
-        int high = numbers.length - 1;
-        int middle;
-        while (low <= high) {
-            middle = (low + high) / 2;
-            if (numbers[middle] < target) {
-                low = middle + 1;
-            } else if (numbers[middle] > target) {
-                high = middle - 1;
+        int lowIndex = 0;
+        int highIndex = numbers.length - 1;
+        int middleIndex;
+        while (lowIndex <= highIndex) {
+            middleIndex = (lowIndex + highIndex) / 2;
+            if (numbers[middleIndex] < target) {
+                lowIndex = middleIndex + 1;
+            } else if (numbers[middleIndex] > target) {
+                highIndex = middleIndex - 1;
             } else {
-                return middle;
+                return numbers[middleIndex];
             }
         }
-        return -1;
+        throw new IllegalStateException();
     }
 
-    public static int binarySearchRecursive(int[] numbers, int target, int low, int high) {
-        if (low > high) return -1;
-        int middle = (low + high) / 2;
-        if (numbers[middle] < target) {
-            return binarySearchRecursive(numbers, target, middle + 1, high);
-        } else if (numbers[middle] > target) {
-            return binarySearchRecursive(numbers, target, low, middle - 1);
+    public static int binarySearchRecursive(int[] numbers, int target, int lowIndex, int highIndex) {
+        if (lowIndex > highIndex) throw new IllegalStateException();
+        int middleIndex = (lowIndex + highIndex) / 2;
+        if (numbers[middleIndex] < target) {
+            return binarySearchRecursive(numbers, target, middleIndex + 1, highIndex);
+        } else if (numbers[middleIndex] > target) {
+            return binarySearchRecursive(numbers, target, lowIndex, middleIndex - 1);
         } else {
-            return middle;
+            return numbers[middleIndex];
         }
     }
 }
